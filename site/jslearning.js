ad707727c42ddce9bf2d602e67265d39ae2a4eff
Antony Titan <antonytitan96@gmail.com>
2023-02-15 15:01:50 +0300

    footer

---
+++ site/jslearning.js
@@ -0,0 +1,16 @@
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.bark = function () {
+  console.log(this.name + " likes barking! Bark!");
+}
+
+var max = new Dog("Max", "Buddy");
+max.bark();
+
+function sayHello() {
+  alert("Hello there");
+  }
+
+sayHello();
